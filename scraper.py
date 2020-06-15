31e8eed027ce5149ff93234e907742bab1a763cd
kubinks <kubinks1@gmail.com>
2020-06-15 11:41:20 +0200

    improved comments

--- scraper.py
+++ scraper.py
@@ -14,11 +14,11 @@ def get_links(url, i=0, pages=0):
     if i == 0:
         pages = int(soup.findAll('a', {'class': 'block br3 brc8 large tdnone lheight24'})[-1].text.strip())
 
-    # looks for a container with all the needed data
+    # looks for a container with offers
     
     for item in soup.findAll('tr', {'class': 'wrap'}):
         
-        # scrapes name, link, number of reviews, cooking and rating
+        # scrapes link for each offer
         
         link = item.td.div.table.tbody.tr.td.a['href']
                    
@@ -27,8 +27,7 @@ def get_links(url, i=0, pages=0):
         if DEBUG:
             print('Link: ', link)
     
-    # we look for the next page, if it is not present we just save list to csv
-
+    # keeps scraping every next page until done, then saves data to .csv
                     
     if i == pages:
         df = pd.DataFrame(links, columns=['Link'])
@@ -50,15 +49,19 @@ def get_links(url, i=0, pages=0):
 def get_offers(url):
     soup = get_soup(url)
     
+    # distinguishes whether an offer is from 'olx' or 'otodom'
+    
     if 'www.olx.pl' in url:
         container = soup.find('div', {'class': 'offerdescription clr'})
         
-        # scrapes reviews one by one, etracting rating and review text itself
+        # scrapes url, offer, its price and other features to a dictionary
+        
         data = {}
         data['Url'] = url
         data['Oferta'] = container.h1.text.strip().replace(',', '.')
-        data['Cena'] = container.find('div', {'class': 'pricelabel'}).text.strip().split('\n')[0])
+        data['Cena'] = container.find('div', {'class': 'pricelabel'}).text.strip().split('\n')[0]
         features = container.findAll('li', {'class': 'offer-details__item'})
+        
         for feature in features:
             key, value = feature.text.strip().split('\n')
             data[key] = value
@@ -68,12 +71,13 @@ def get_offers(url):
         if DEBUG:
             print(data)
     else:
-        # scrapes reviews one by one, etracting rating and review text itself
+        # scrapes url, offer, its price and other features to a dictionary
         data = {}
         data['Url'] = url
         data['Oferta'] = soup.find('div', {'class': 'css-d2oo9m'}).h1.text.strip().replace(',', '.')
-        data['Cena'] = soup.find('div', {'class': 'css-1vr19r7'}).text.strip().split('/')[0].strip().split('\n')[0])
+        data['Cena'] = soup.find('div', {'class': 'css-1vr19r7'}).text.strip().split('/')[0].strip().split('\n')[0]
         features = soup.find('div', {'class': 'css-1ci0qpi'}).findChildren()
+        
         for i,feature in enumerate(features):
             if i%2 != 0:
                 key, value = feature.text.strip().split(':')
@@ -86,20 +90,22 @@ def get_offers(url):
         
             
 
-# set scraped website, debug state and ranks dictionary          
+# set scraped website, debug state         
 DEBUG = True
 base_url = 'https://www.olx.pl/nieruchomosci/mieszkania/wynajem/lodz/?search%5Bfilter_enum_builttype%5D%5B0%5D=blok'
 links = []
 offers = []
 
-# scrape restaurants
+# scrape links
 get_links(base_url)
 
-# read csv with restaurants and scrape reviews of each one
+# read .csv with apartments and scrape data of each one
 df = pd.read_csv('apartments.csv')
 
 for url in tqdm(df.Link):
     get_offers(url)
+    
+# saves the list of offers to .csv file
     
 df = pd.DataFrame(offers)
 df.to_csv('offers.csv')
